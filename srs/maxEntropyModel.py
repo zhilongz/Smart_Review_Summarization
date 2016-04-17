d1a3cb0e49f4668aaeba9d03eaa8a3b74ec5b57c
KEHANG <kehanghan@gmail.com>
2016-04-17 07:54:41 -0400

    change dynamic_aspect_list mode to wordlist_dict mode
    also cleaned up the code

--- srs/maxEntropyModel.py
+++ srs/maxEntropyModel.py
@@ -13,24 +13,20 @@ from nltk.stem import SnowballStemmer
 from utilities import Sentence
 
 
-def loadDynamicAspectList():
+def loadWordListDict(word_list_dict_path):
     #load a list of features words extracted and acts as the dynamic aspect. The length of this list will determine the size of the model
-    dynamic_aspect_list = [u'camera', u'pictur', u'batteri', u'qualiti', \
-    u'zoom', u'shot', u'time', u'lif', u'photo', u'featur', u'memori', \
-    u'card', u'imag', u'review', u'flash', \
-    u'resolut', u'set', u'cabl', u'problem', u'screen', \
-    u'adapt', u'size', u'photograph', \
-    u'light', u'softwar', u'pic', u'screen', u'price', u'mode', \
-    u'scene', u'color', u'point', u'angl',u'button', u'port']
-    return dynamic_aspect_list
+    import json
+    wordlist_dict = json.load(open(word_list_dict_path, 'r'))
 
-def eval_f_vec(sentence): 
+    print "wordlist_dict has {0} keys.".format(len(wordlist_dict))
+
+    return wordlist_dict
+
+def eval_f_vec(sentence, wordlist_dict): 
     """
     sentence: a single labelled sentence obj 
-    returns a vector of length len(dynamic_aspect_list)
+    returns a vector of length len(wordlist_dict)
     """
-    dynamic_aspect_list = loadDynamicAspectList()   
-
     word_list = []
     if sentence.tokens != []:
         word_list = sentence.tokens
@@ -52,12 +48,16 @@ def eval_f_vec(sentence):
     len_word = max(len(word_list),1)*1.0;
     
     f_vec = []
-    for w in dynamic_aspect_list:
-        f_vec.append(word_list.count(w)/len_word)
+    static_aspect_list = sorted(wordlist_dict.keys())
+    for key in static_aspect_list:
+        count = 0
+        for represent_word in wordlist_dict[key]:
+            count += word_list.count(represent_word)
+        f_vec.append(count/len_word)
     
     return f_vec
         
-def cond_prob(lam_vec,static_aspect_list,sentence, isTraining=True): 
+def cond_prob(lam_vec, wordlist_dict, static_aspect_list,sentence, isTraining=True): 
     """ 
     lam_vec: lambda as a vector  
     static_aspect_list: list of all static features 
@@ -65,7 +65,7 @@ def cond_prob(lam_vec,static_aspect_list,sentence, isTraining=True):
         
     returns the conditional probability for all class (a vector)
     """
-    f = eval_f_vec(sentence)
+    f = eval_f_vec(sentence, wordlist_dict)
     f_len = len(f)
 
     numerators = []
@@ -91,7 +91,7 @@ def cond_prob(lam_vec,static_aspect_list,sentence, isTraining=True):
 
     return cp 
 
-def loss_func(lam_vec,static_aspect_list,ls_list):
+def loss_func(lam_vec,wordlist_dict, static_aspect_list,ls_list):
     """ 
     loss function
     First argument is parameter vector to be optimized  
@@ -99,7 +99,7 @@ def loss_func(lam_vec,static_aspect_list,ls_list):
 
     loss = 0
     for ls in ls_list: 
-        f = eval_f_vec(ls)
+        f = eval_f_vec(ls, wordlist_dict)
         f_len = len(f)
         
         labeled_aspect_idx = 0
@@ -109,7 +109,8 @@ def loss_func(lam_vec,static_aspect_list,ls_list):
             if c == ls.labeled_aspects:
                 labeled_aspect_idx = static_aspect_list.index(c)
                 deno = deno + np.exp(np.inner(lam_vec[labeled_aspect_idx*f_len:(labeled_aspect_idx+1)*f_len],f))
-            deno = deno + 1
+            else:
+                deno = deno + 1
         
         loss_sent = np.inner(lam_vec[labeled_aspect_idx*f_len:(labeled_aspect_idx+1)*f_len],f) - np.log(deno)
         loss = loss + loss_sent
@@ -117,18 +118,42 @@ def loss_func(lam_vec,static_aspect_list,ls_list):
     print loss*(-1)    
     return loss*(-1)
 
-def train(static_aspect_list,ls_list,lambda_len):
+def generateInitialGuess(lambda_len, static_aspect_list_len=None):
+    if static_aspect_list_len:
+        lambda_guess = np.zeros(lambda_len)
+        for i in range(static_aspect_list_len):
+            diagonal_idx = i + static_aspect_list_len*i
+            lambda_guess[diagonal_idx] = np.random.randn()
+    else:
+        lambda_guess = np.random.rand(lambda_len)
+
+    return lambda_guess
+
+def train(wordlist_dict, static_aspect_list,ls_list,lambda_len):
     """ optimization by minimizing the loss function returns lambda* """
-    lambda_guess = np.random.rand(lambda_len)*0.1
-    res = minimize(loss_func, lambda_guess, args=(static_aspect_list,ls_list), method='BFGS', options={'gtol': 1e-3, 'disp': True})
+    lambda_guess = generateInitialGuess(lambda_len, len(static_aspect_list))
+    res = minimize(loss_func, lambda_guess, args=(wordlist_dict, static_aspect_list,ls_list), method='BFGS', options={'gtol': 1e-3, 'disp': True})
     return res
     """
     see for list of method: http://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.optimize.minimize.html
     """
 
-def loadTrainingData():
+def loadUsefulTrainingData(static_traning_data_dir):
     import os
-    static_traning_data_dir = os.path.abspath('static_training_data/')
+    sentences = []
+    for data_file in os.listdir(static_traning_data_dir):
+        if data_file.endswith('labeled.txt'):
+            sentences.extend(load_labelled_sent(os.path.join(static_traning_data_dir, data_file)))
+    
+    useful_sentences = []
+    for sent in sentences:
+        if sent.labeled_aspects not in ['no feature', 'other features']:
+            useful_sentences.append(sent)
+
+    return useful_sentences
+
+def loadTrainingData(static_traning_data_dir):
+    import os
     sentences = []
     for data_file in os.listdir(static_traning_data_dir):
         if data_file.endswith('labeled.txt'):
@@ -142,53 +167,6 @@ def load_labelled_sent(file_name):
             line_splitted = line.split('***')
             ls_list.append(Sentence(content=line_splitted[1],labeled_aspects=line_splitted[0]))
     return ls_list 
-
-def main():
-    # load a list of features words extracted and acts as the dynamic aspect. The length of this list will determine the size of the model
-    dynamic_aspect_list = loadDynamicAspectList()
-
-    # load a list of classes (static aspect)
-    static_aspect_list = ['price','pictures','video','zoom','size','design','battery','screen','detection','ease of use','quality','other features','no feature']
-    
-    # load labeled sentences as a list of labeled_sent
-    ls_list = load_labelled_sent('static_training_data/B00B7O2Z42_labeled.txt')
-    
-    # train 
-
-    n = len(dynamic_aspect_list)*len(static_aspect_list)
-    res = train(static_aspect_list,ls_list[:180],n)
-    
-    lambda_star = res.x 
-    print 'The lambda is: \n'
-    print lambda_star
-    
-    #load unlabeled testing data (TBD) 
-    uls_list = ls_list[180:]    
-    
-    #validation 
-    correct = 0.0
-    for uls in uls_list:
-        cp = cond_prob(lambda_star,static_aspect_list,uls, isTraining=False)
-        predicted_class_index = cp.index(max(cp)) 
-        if static_aspect_list[predicted_class_index] == uls.labeled_aspects:
-            correct += 1
-    
-    class_error = 1.0 - correct/len(uls_list)
-    print 'The classification error is: %.2f' % (class_error)
-
-def testcase():
-    # load a list of classes (static aspect)
-    static_aspect_list = ['price','pictures','video','zoom','size','design','battery','screen','detection','ease of use','quality','other features','no feature']
-    
-    # load labeled sentences as a list of labeled_sent
-    ls_list = load_labelled_sent('static_training_data/B00B7O2Z42_labeled.txt')
-    print loadDynamicAspectList()
-    print eval_f_vec(ls_list[1])
-    print static_aspect_list[1]
-    print len(ls_list)
-
-if __name__ == '__main__':
-    testcase()
 
     
     
