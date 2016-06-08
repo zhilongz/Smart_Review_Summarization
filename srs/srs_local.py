49b83795a1eb143a5082b438e02316ff3980db8f
KEHANG <kehanghan@gmail.com>
2016-06-07 23:05:06 -0400

    srs_local serves as a workflow of srs
    - if no entry in db, fill in db first
    
    - then do plotting

--- srs/srs_local.py
+++ srs/srs_local.py
@@ -1,31 +1,62 @@
 from scraper import main as scraper_main
-from swnModel import swnModel
+from predictor import StaticPredictor, loadTrainedStaticPredictor
 from srs import settings
+from utilities import loadScraperDataFromDB, Sentence
+from swnModel import get_sentiment_score_for_sentences, get_ftScore_ftSenIdx_dicts
+from sentiment_plot import box_plot
+from database import insert_for_product_id, update_for_product_id
 import os
 
-def main(prod1ID, prod2ID, scrapeFlag=True):
-	
-	# scrap data
-	if scrapeFlag:
-		scraper_main(prod1ID)
-		if prod2ID != None:
-			scraper_main(prod2ID)
+def get_ft_dicts_from_contents(contents, staticPredictor):
+	sentences = []
+	for cont in contents:
+		sentences.append(Sentence(content=cont))
 
-	# prepare static predictor params  
-	params_filename = 'lambda_opt_regu2.txt'
-	wordlist_filename = 'wordlist_dict_1.txt'
+	staticPredictor.predict_for_sentences(sentences)	
+	get_sentiment_score_for_sentences(sentences)
+	return get_ftScore_ftSenIdx_dicts(sentences)
 	
-	# sentiment analysis and plot
-	plot_folder = settings['sentiment_plot']
-	if prod2ID == None:
-		figure_file_path = os.path.join(plot_folder, prod1ID + '_boxplot.png')
-		swnModel(params_filename,wordlist_filename,figure_file_path,prod1ID)
+
+def fill_in_db(product_id):
+	
+	# fetch product info from db 
+	prod1_contents, prod1_ft_score_dict, prod1_ft_senIdx_dict = loadScraperDataFromDB(product_id)
+
+	if len(prod1_contents) == 0: # not in db yet
+		# scrape contents
+		prod1_contents = scraper_main(product_id)
+		
+		# classify, sentiment score
+		staticPredictor = loadTrainedStaticPredictor()
+		prod1_ft_score_dict, prod1_ft_senIdx_dict = \
+		get_ft_dicts_from_contents(prod1_contents, staticPredictor)
+		
+		# insert new entry
+		insert_for_product_id(product_id, prod1_contents, \
+			prod1_ft_score_dict, prod1_ft_senIdx_dict)
+	
+	elif len(prod1_ft_score_dict) == 0 or len(prod1_ft_senIdx_dict) == 0:
+		
+		# classify, sentiment score
+		staticPredictor = loadTrainedStaticPredictor()
+		prod1_ft_score_dict, prod1_ft_senIdx_dict = \
+		get_ft_dicts_from_contents(prod1_contents, staticPredictor)
+		
+		# update old entry
+		update_for_product_id(product_id, prod1_ft_score_dict, prod1_ft_senIdx_dict)
 	else:
-		figure_file_path = os.path.join(plot_folder, prod1ID + '_'+ prod2ID + '_boxcompare.png')
-		swnModel(params_filename,wordlist_filename,figure_file_path,prod1ID,prod2ID)
+		print "Filled in db before for {0}".format(product_id)
+	
+def plot(product_id):
+	_, prod1_ft_score_dict, _ = loadScraperDataFromDB(product_id)
+	plot_folder = settings['sentiment_plot']
+	figure_file_path = os.path.join(plot_folder, product_id + '_boxplot.png')
+	box_plot(prod1_ft_score_dict, figure_file_path, product_id)
+
+def main(product_id):
+	fill_in_db(product_id)
+	plot(product_id)
 
 if __name__ == '__main__':
-	prod1ID = 'B00I8BICB2'
-	prod2ID = 'B00HZE2PYI'
-	scrapeFlag = False
-	main(prod1ID, prod2ID, scrapeFlag)
+	product_id = 'B00HZE2PYI'
+	main(product_id)
