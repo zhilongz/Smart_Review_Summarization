8d6cc6b5cf2d1344545ee3b485a55f12273163be
KEHANG <kehanghan@gmail.com>
2016-06-28 22:20:51 -0400

    sentimentBoxPlot code clean-up
    separate data preparation and plotting

--- srs/sentiment_plot.py
+++ srs/sentiment_plot.py
@@ -5,122 +5,176 @@ from bokeh.models import ColumnDataSource, TapTool, CustomJS, HoverTool
 import numpy as np
 from matplotlib import pyplot as plt
 
-def sentimentBoxPlot(contents, feature_scorelist_dict, feature_senIdxlist_dict):
+def prepareDataForRectPlot(feature_scorelist_dict, sort=True, spanTop=75, spanBottom=25):
+	"""
+	INPUT: feature_scorelist_dict
+	OUTPUT: mids, spans
+	"""
 
-	features = []
+	features = feature_scorelist_dict.keys()
+	if sort:
+		features = sorted(features)
+	
 	mids = []
 	spans = []
-	all_score_list = []
-	color1 = "#F2583E"
-	color_hover = "olive"
-
-	features = sorted(feature_scorelist_dict.keys())
-
 	for feature in features:
 		scorelist = feature_scorelist_dict[feature]
 		scorearray = np.array(scorelist)
 		mean = np.mean(scorearray)
-		span = np.percentile(scorearray, 75) - np.percentile(scorearray, 25)
+		span = np.percentile(scorearray, spanTop) - np.percentile(scorearray, spanBottom)
 
 		mids.append(mean)
 		spans.append(span)
-		all_score_list.append(scorelist)
 
-	TOOLS = "save"
-	p = figure(tools=TOOLS, plot_width=700, plot_height=470, x_range=features, title_text_font_size='16pt',y_axis_label="Sentiment Score")
-	p.yaxis.axis_label_text_font_size = "12pt"
-	p.xaxis.axis_label_text_font_size = "12pt"
-	w = 0.3
-	p_rect=p.rect(features, mids, w, spans, fill_color=color1, line_color="black",hover_color=color_hover, hover_alpha=1.0)
-	
-	p.title = "Product Review Aspect Summary"
-	p.xaxis.major_label_orientation = pi/4
-	p.grid.grid_line_alpha=0.3
-	p.logo = None
+	return mids, spans
 
-	p2 = figure(tools="save", title="Aspect Histogram", plot_width=400, plot_height=270,title_text_font_size='16pt',y_axis_label='# of sentences',x_axis_label="Sentiment Score")
-	s2=ColumnDataSource(data=dict(top=[], bottom=[], left=[], right=[], feature=''))
-	p2_quad = p2.quad(top='top',bottom='bottom',left='left',right='right', source=s2,line_width=2,fill_alpha=0.8,color=color_hover)
-	p2.xaxis.axis_label_text_font_size = "12pt"
-	p2.yaxis.axis_label_text_font_size = "12pt"
-	p2.logo = None
+def prepareDataForHistPlotAndSampleSentences(
+	feature_scorelist_dict, feature_senIdxlist_dict, contents, sort=True, exampleSentences_size=3):
+	"""
+	INPUT: feature_scorelist_dict, feature_senIdxlist_dict, contents
+	OUTPUT: histTops, histLefts, histRights, sampleSentences_dict
+	"""
 
-	Hover_jscode="""
-		//get score list data
-		var all_score_list = all_score_list_data.get('data')['all_score_list'];
+	features = feature_scorelist_dict.keys()
+	if sort:
+		features = sorted(features)
 
-		//get the source data for Fig.2
-		var fig2data = s2.get('data');
-		var features = features_data.get('data')['features'];
-
-		//get current hovering index
-		fillHistData(cb_data, fig2data, features, all_score_list);		
-		s2.trigger('change');
-        
-		"""
-
-	all_score_list_data = ColumnDataSource(data=dict(all_score_list=all_score_list))
-	features_data = ColumnDataSource(data=dict(features=features))
-
-	Hover_CallBack=CustomJS(args={'s2':s2, 'features_data':features_data, 'all_score_list_data':all_score_list_data},code=Hover_jscode)
-	p.add_tools(HoverTool(renderers=[p_rect],callback=Hover_CallBack,tooltips=None))
-
-	# get sentences for each bin of histogram
-	feature_example_sen_dict = {}
-	for feature, scorelist in feature_scorelist_dict.items():
+	histTops = []
+	histLefts = []
+	histRights = []
+	sampleSentences_dict = {}
+	for feature in features:
+		# get raw data: scorelist
+		scorelist = feature_scorelist_dict[feature]
+		senIdxlist = feature_senIdxlist_dict[feature]
 
 		score_num = len(scorelist)
 		min_score = min(scorelist)
 		max_score = max(scorelist)
 		
+		# determine bin_num
 		bin_num = 4
 		if score_num <= 10: bin_num = 2
 		elif score_num <= 30: bin_num = 3
 		elif score_num <= 100: bin_num = 4
 		else: bin_num = 5
 
-		feature_example_sen_dict[feature] = {}
-		for bin0 in range(bin_num):
-			feature_example_sen_dict[feature][bin0] = []
+		# initialize histTop
+		histTop = [0 for _ in range(bin_num)]
+		exampleSentences = [[] for _ in range(bin_num)]
 
 		delimiter = np.linspace(min_score, max_score, bin_num+1)
-		for i, score in enumerate(scorelist):
-
-			senIdx = feature_senIdxlist_dict[feature][i]
-			content = contents[senIdx]
-
+		
+		for senIdx, score in zip(senIdxlist, scorelist):
 			for j in range(bin_num):
 				if score >= delimiter[j] and score <= delimiter[j+1]:
-					example_sen = feature_example_sen_dict[feature][j]
-					if len(example_sen) < 3:
-						example_sen.append(content)
-					else:
-						continue
+					histTop[j] += 1
+					if len(exampleSentences[j]) < exampleSentences_size:
+						content = contents[senIdx]
+						exampleSentences[j].append(content)
+
 					break
+		histLeft = list(delimiter[0:bin_num])
+		histRight = list(delimiter[1:bin_num+1])
+		
+		histTops.append(histTop)
+		histLefts.append(histLeft)
+		histRights.append(histRight)
+		sampleSentences_dict[feature] = exampleSentences
 
+	hist_cds = ColumnDataSource(data=dict(tops=histTops, lefts=histLefts, rights=histRights, features=features))
+	sampleSentences_cds = ColumnDataSource(data=sampleSentences_dict)
 
-	s3 = ColumnDataSource(data=feature_example_sen_dict)
+	return hist_cds, sampleSentences_cds
 
-	Hover_jscode2="""
-		var feature_example_sen_dict = s3.get('data');
-		var feature = s2.get('data')['feature'];
-		var barIdx = cb_data.index['1d'].indices[0];
+def getRectPlot(features, mids, spans, w=0.3, fill_color="#2ca25f", hover_color="#99d8c9",
+	plot_width=650, plot_height=450, major_label_orientation=pi/4, 
+	grid_line_alpha=0.3, axis_label_text_font_size='12pt'):
 
-		if (typeof barIdx != 'undefined'){
-		var example_sen = feature_example_sen_dict[feature][barIdx];
-		console.log(example_sen[0]);
-		document.getElementsByClassName("sample_reviews")[0].innerHTML = "Sample reviews:" + example_sen[0];
-		}
-		else {
-		document.getElementsByClassName("sample_reviews")[0].innerHTML = "Sample reviews:";
-		}
+	# plot rectPlot
+	rectPlot = figure(tools="", plot_width=plot_width, plot_height=plot_height, x_range=features,y_axis_label="Sentiment Score")
+	rectPlot.yaxis.axis_label_text_font_size = axis_label_text_font_size
+	rectPlot.xaxis.axis_label_text_font_size = axis_label_text_font_size
+	rectPlot.xaxis.major_label_orientation = major_label_orientation
+	rectPlot.grid.grid_line_alpha=grid_line_alpha
+	rectPlot.logo = None
+	rectPlot.toolbar_location = None
+
+	rectPlot_rect=rectPlot.rect(features, mids, w, spans, fill_color=fill_color, line_color=fill_color,hover_color=hover_color, hover_alpha=1.0)
+
+	return rectPlot, rectPlot_rect
+
+def getHistPlot(histPlot_cds, color="#99d8c9",
+	grid_line_alpha=0.3, axis_label_text_font_size='12pt'):
+
+	histPlot = figure(tools="", plot_width=400, plot_height=250,y_axis_label='Comments',x_axis_label="Sentiment Score")
+	histPlot.xaxis.axis_label_text_font_size = axis_label_text_font_size
+	histPlot.yaxis.axis_label_text_font_size = axis_label_text_font_size
+	histPlot.logo = None
+	histPlot.toolbar_location = None
+
+	# initialize plot
+	histPlot_quad = histPlot.quad(top='top',bottom='bottom',left='left',right='right', source=histPlot_cds,line_width=2,fill_alpha=0.8,color=color)
+
+	return histPlot, histPlot_quad
+
+def getInitialHistPlotData(hist_cds):
+
+	top = hist_cds.data["tops"][0]
+	left = hist_cds.data["lefts"][0]
+	right = hist_cds.data["rights"][0]
+	bottom = [0 for _ in top]
+	feature = hist_cds.data["features"][0]
+	histPlot_data = dict(top=top, bottom=bottom, left=left, right=right, feature=feature)
+	histPlot_cds=ColumnDataSource(data=histPlot_data)
+
+	return histPlot_cds
+
+def sentimentBoxPlot(contents, feature_scorelist_dict, feature_senIdxlist_dict):
+
+	# get data for rectPlot
+	features = sorted(feature_scorelist_dict.keys())
+	mids, spans = prepareDataForRectPlot(feature_scorelist_dict, sort=True)
+
+	# get data for histPlot and sample reviews
+	hist_cds, sampleSentences_cds = prepareDataForHistPlotAndSampleSentences(
+	feature_scorelist_dict, feature_senIdxlist_dict, contents, sort=True)
+
+	# plot rectPlot
+	rectPlot, rectPlot_rect = getRectPlot(features, mids, spans)
+
+	# plot histPlot
+	histPlot_cds = getInitialHistPlotData(hist_cds)
+	histPlot, histPlot_quad = getHistPlot(histPlot_cds)
+
+	# add interaction between rectPlot and histPlot
+	hoverJS="""
+		var histPlot_data = histPlot_cds.get('data');
+		var features = hist_cds.get('data')['features'];
+		var hist_data = hist_cds.get('data');
+
+		//get current hovering index
+		fillHistData(cb_data, histPlot_data, features, hist_data);		
+		histPlot_cds.trigger('change');
+        
+		"""
+
+	hoverCallBack=CustomJS(args={'histPlot_cds':histPlot_cds, 'hist_cds':hist_cds},code=hoverJS)
+	rectPlot.add_tools(HoverTool(renderers=[rectPlot_rect],callback=hoverCallBack,tooltips=None))
+
+	# add interaction between histPlot and sample reviews
+	hoverJS2="""
+		var sampleSentences_dict = sampleSentences_cds.get('data');
+		
+		var feature = histPlot_cds.get('data')['feature'];
+		fillSampleReviews(cb_data, sampleSentences_dict, feature);
         
 		""" 
 
-	Hover_CallBack2=CustomJS(args={'s3':s3, 's2': s2}, code=Hover_jscode2)
-	p2.add_tools(HoverTool(renderers=[p2_quad],callback=Hover_CallBack2,tooltips=None))
+	hoverCallBack2=CustomJS(args={'sampleSentences_cds':sampleSentences_cds, 'histPlot_cds': histPlot_cds}, code=hoverJS2)
+	histPlot.add_tools(HoverTool(renderers=[histPlot_quad],callback=hoverCallBack2,tooltips=None))
 
-	return (p,p2)
+	return (rectPlot,histPlot)
 
 def sentimentBoxPlot_Compare(feature_scorelist_dict,feature_scorelist_dict2):
 
